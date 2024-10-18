60da67a22aa270f7bb9de89c3ba0bf26642ebc93
umme-f <ummefatema5995@gmail.com>
2024-10-18 09:19:42 +0900

    changed suppliers code

--- src/components/MoreInformation.jsx
+++ src/components/MoreInformation.jsx
@@ -30,9 +30,6 @@ const MoreInformation = () => {
   const toVehicleDetails = () => {
     navigate("/vehicle-details");
   };
-  const toSupplierDetails = () => {
-    navigate("/supplier-details");
-  };
 
   return (
     <div className="flex justify-center items-center min-h-screen">
@@ -73,14 +70,6 @@ const MoreInformation = () => {
           </div>
         </div>
 
-<<<<<<< HEAD
-        {/* Supplier Details */}
-        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white p-4">
-            {t("Supplier Details")}
-          </h5>
-
-=======
         {/* Supplier details */}
         <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
@@ -88,19 +77,16 @@ const MoreInformation = () => {
               {t("Supplier Details")}
             </h5>
           </a>
->>>>>>> 91bc56d8c2d990cc47e5196fc285740c0e77138a
           {/* Center the button */}
           <div className="flex justify-center">
-            <button
-              onClick={toSupplierDetails}
+            <a
+              href="#"
               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              {t("ReadMore")}
-            </button>
+              {t("Read more")}
+            </a>
           </div>
         </div>
-
-        
 
         {/* User details */}
         <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
